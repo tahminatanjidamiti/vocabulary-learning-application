a1312770b82bb7f53f2c7e000a32abb97df5b01b
tahminatanjidamiti <mity2027@gmail.com>
2024-11-22 19:13:30 +0600

    added id wise card

--- src/components/SingleLesson.jsx
+++ src/components/SingleLesson.jsx
@@ -9,6 +9,25 @@ const SingleLesson = () => {
     } = data[0];
 
     
+
+    return (
+        <div>
+            <div className='w-80 mx-auto py-10'> 
+            <div className="card bg-base-100 shadow-xl border-2">
+                <div className="card-body text-center">
+                    <h2 className="card-title flex justify-center items-center">{word}</h2>
+                    <h2 className="card-title flex justify-center items-center">{pronunciation}</h2>
+                    <p>{meaning}</p>
+                    <p>{part_of_speech}</p>
+                    <div className="card-actions justify-center">
+                        <button className="btn btn-info">Learn More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Link to="/start_learning" className="btn w-8/12 mx-auto flex justify-center items-center btn-info my-10">Back To Lesson</Link>
+        </div>
+    );
 };
 
 export default SingleLesson;
